97a199f63555ff9a0ee2a60fdd29f032cb9d02d4
Nicolas Bernal <nicolasbernal105@gmail.com>
2024-05-26 15:21:50 -0500

    Actualizacion Models API

--- usuario/usuario.model.js
+++ usuario/usuario.model.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const schemaUsuario = new mongoose.Schema({
     correo: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true },
     password: { type: String, required: true },
-    isActive: { type: Boolean, required: true, default: true } // Soft Delete
+    isActive: { type: Boolean, required: true, default: true } // Soft Delete 
 }, {
     versionKey: false,
     timestamps: true
+
 });
 
 const Usuario = mongoose.model('Usuario', schemaUsuario);
